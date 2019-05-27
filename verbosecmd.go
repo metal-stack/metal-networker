67b11ddcd21c67dffb67f40540e29ed010174f70
Tobias Suckow <ize0j10@f-i-ts.de>
2019-05-27 06:48:51 +0200

    Improved documentation.

--- verbosecmd.go
+++ verbosecmd.go
@@ -6,15 +6,18 @@ import (
 	"os/exec"
 )
 
+// VerboseCmd represents a system command with verbose output.
 type VerboseCmd struct {
 	Cmd exec.Cmd
 }
 
+// NewVerboseCmd creates a new instance of VerboseCmd.
 func NewVerboseCmd(name string, args ...string) VerboseCmd {
 	cmd := exec.Command(name, args...)
 	return VerboseCmd{*cmd}
 }
 
+//Run executes the command and returns any errors in case exist.
 func (v VerboseCmd) Run() error {
 	var stderr bytes.Buffer
 	v.Cmd.Stderr = &stderr
