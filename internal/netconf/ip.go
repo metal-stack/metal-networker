ce0310235f2a50976ec2a05b7a942c6c37b77bf2
Tobias <ize0j10@f-i-ts.de>
2019-07-26 08:57:33 +0200

    Re-introduce separation of error handling and functional check to increase readability.

--- internal/netconf/ip.go
+++ internal/netconf/ip.go
@@ -6,7 +6,10 @@ func getLocalBGPIP(primary Network) string {
 	firstIP := net.ParseIP(primary.Ips[0])
 	for _, prefix := range primary.Prefixes {
 		ip, network, err := net.ParseCIDR(prefix)
-		if err == nil && network.Contains(firstIP) {
+		if err != nil {
+			continue
+		}
+		if network.Contains(firstIP) {
 			// Set the last octet to "0" regardless of version
 			ip[len(ip)-1] = 0
 			return ip.String()
