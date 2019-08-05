c12cd438c098e571c549138233ff565cc674886f
Tobias Suckow <ize0j10@f-i-ts.de>
2019-08-05 11:42:25 +0200

    Removal unused variable.

--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"git.f-i-ts.de/cloud-native/metal/metal-networker/cmd"
-	"git.f-i-ts.de/cloud-native/metallib/zapup"
 )
-
-var log = zapup.MustRootLogger().Sugar()
 
 func main() {
 	cmd.Execute()
