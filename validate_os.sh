d752c82805dd1d027934888fae6757bcd01055ce
Markus Fensterer <markus.fensterer@gmail.com>
2020-07-27 17:57:10 +0200

    remove test data validation for ifaces files because systemd-networkd does not have such things

--- validate_os.sh
+++ validate_os.sh
@@ -6,8 +6,3 @@ for input in ${FRR_FILES}; do
     echo "/testdata/${input}"
     vtysh --dryrun --inputfile "/testdata/${input}"
 done
-
-for input in ${INTERFACES_FILES}; do
-    echo "/testdata/${input}"
-    test -z $(ifup --syntax-check --all -i "/testdata/${input}" 2>&1 | grep -v "syslogs:")
-done
