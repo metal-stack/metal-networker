8cda1a671327576b95e631fc0b626c9bdfb213b3
Stefan Majer <stefan.majer@f-i-ts.de>
2022-04-25 10:04:19 +0200

    Build with go-1.18, test debian 11, fix validate condition (#74)

--- validate_os.sh
+++ validate_os.sh
@@ -2,11 +2,13 @@
 
 testcases="/testdata/frr.conf.*"
 for tc in $testcases; do
-    print "Testing FRR ${FRR_VERSION} on ${OS_NAME}:${OS_VERSION} with input ${tc}: "
-    if ! vtysh --dryrun --inputfile "${tc}";
+    echo -n  "Testing FRR ${FRR_VERSION} on ${OS_NAME}:${OS_VERSION} with input ${tc}: "
+    if vtysh --dryrun --inputfile "${tc}";
     then
         printf "\e[32m\xE2\x9C\x94\e[0m\n"
     else
         printf "\e[31m\xE2\x9D\x8C\e[0m\n"
+        echo "FRR ${FRR_VERSION} on ${OS_NAME}:${OS_VERSION} produces an invalid configuration"
+        exit 1
     fi
 done
