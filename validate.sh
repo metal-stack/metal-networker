6f2e5aae10f863ebe967a65c2994e352428d5f63
Markus Fensterer <markus.fensterer@gmail.com>
2024-11-12 09:34:12 +0100

    validate nftables rule files

--- validate.sh
+++ validate.sh
@@ -6,12 +6,22 @@ validate () {
     echo "----------------------------------------------------------------"
     echo "Validating sample artifacts of metal-networker with ${1}:${2} frr:${3}"
     echo "----------------------------------------------------------------"
+    tag="${1}_${2}_${3}"
     docker build \
         --build-arg OS_NAME="${1}" \
         --build-arg OS_VERSION="${2}" \
         --build-arg FRR_VERSION="${3}" \
         --file Dockerfile.validate \
-        . -t metal-networker
+        . -t metal-networker-validate:${tag}
+
+    docker run --interactive \
+        --rm \
+        --network=none \
+        --cap-add=NET_ADMIN \
+        --cap-add=NET_RAW \
+        --name vali \
+        --volume ./pkg/netconf/testdata:/testdata \
+        metal-networker-validate:${tag} /validate_os.sh
 }
 
 validate "ubuntu" "24.04" "frr-10"
